018c7aee6c62b61db0181e5128c73c2234afdaae
Dmitriy-Knyazev <gala50002@mail.ru>
2022-11-19 11:42:18 +0300

    Добавил к тестам Маркеры: xfail и parametrize

--- tests/api/test_api_chuck_norris.py
+++ tests/api/test_api_chuck_norris.py
@@ -10,13 +10,14 @@ class Test_api_chuck_norris:
                   "fashion", "food", "history", "money", "movie", "music",
                   "political", "religion", "science", "sport", "travel"]
 
+    expected_value = ['categories', 'created_at', 'icon_url', 'id', 'updated_at', 'url', 'value']
+
 
     """Проверка случайной шутки"""
     def test_api_random_jokes(self):
         random_jokes = 'https://api.chucknorris.io/jokes/random'
         response = Base_methods.api_get(random_jokes)
-        Base_methods.check_json_params(response, ['categories', 'created_at',
-                                                  'icon_url', 'id', 'updated_at', 'url', 'value'])
+        Base_methods.check_json_params(response, Test_api_chuck_norris.expected_value)
         Base_methods.check_json_words_in_value(response, 'value', 'Chuck Norris')
 
 
@@ -28,13 +29,13 @@ class Test_api_chuck_norris:
 
 
     """Проверка GET запросов со всеми категориями"""
+    @pytest.mark.xfail(reason='Иногда Тест падает, не всегда в ответе есть имя "Chuck Norris"')
     def test_api_check_value_categories(self):
         for our_categories in Test_api_chuck_norris.categories:
             sub_url = 'https://api.chucknorris.io/jokes/random?category='
             main_url = sub_url + our_categories
             response = Base_methods.api_get(main_url)
-            Base_methods.check_json_params(response, ['categories', 'created_at',
-                                                      'icon_url', 'id', 'updated_at', 'url', 'value'])
+            Base_methods.check_json_params(response, Test_api_chuck_norris.expected_value)
             Base_methods.check_json_words_in_value(response, 'value', 'Chuck Norris')
 
 
@@ -45,3 +46,15 @@ class Test_api_chuck_norris:
         main_url = sub_url + our_word
         response = Base_methods.api_get(main_url)
         Base_methods.check_value_in_parameter(response, 'total', 35)
+
+
+    """Один Тест с маркером parametrize"""
+    @pytest.mark.parametrize('categories', [
+        "animal", "career", "celebrity", "dev", "explicit",
+        "fashion", "food", "history", "money", "movie", "music",
+        "political", "religion", "science", "sport", "travel"])
+    def test_one_test_with_parametrize(self, categories):
+        category_url = f'https://api.chucknorris.io/jokes/random?category={categories}'
+        response = Base_methods.api_get(category_url)
+        Base_methods.check_json_params(response, Test_api_chuck_norris.expected_value)
+        Base_methods.check_json_words_in_value(response, 'value', 'Chuck Norris')
